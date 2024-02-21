0c66548b89ded2d72e7875a83f76ac011c6350fe
Stivin <stivinmavelil@gmail.com>
2024-02-21 17:26:26 +0100

    Added classes for Manager, Engineer, and Intern that extend the Employee class. Each class has its own constructor that takes in the required parameters and sets the role property accordingly. Also exported the classes at the end of each file.

--- starter/index.js
+++ starter/index.js
@@ -13,3 +13,157 @@ const render = require("./src/page-template.js");
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
+// Create an array to store the team members
+const teamMembers = [];
+
+// Function to add a manager
+const addManager = () => {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "Enter the manager's name:",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "Enter the manager's ID:",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "Enter the manager's email:",
+      },
+      {
+        type: "input",
+        name: "officeNumber",
+        message: "Enter the manager's office number:",
+      },
+    ])
+    .then((answers) => {
+      const manager = new Manager(
+        answers.name,
+        answers.id,
+        answers.email,
+        answers.officeNumber
+      );
+      teamMembers.push(manager);
+      menu();
+    });
+};
+
+// Function to add an engineer
+const addEngineer = () => {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "Enter the engineer's name:",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "Enter the engineer's ID:",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "Enter the engineer's email:",
+      },
+      {
+        type: "input",
+        name: "github",
+        message: "Enter the engineer's GitHub username:",
+      },
+    ])
+    .then((answers) => {
+      const engineer = new Engineer(
+        answers.name,
+        answers.id,
+        answers.email,
+        answers.github
+      );
+      teamMembers.push(engineer);
+      menu();
+    });
+};
+
+// Function to add an intern
+const addIntern = () => {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "Enter the intern's name:",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "Enter the intern's ID:",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "Enter the intern's email:",
+      },
+      {
+        type: "input",
+        name: "school",
+        message: "Enter the intern's school:",
+      },
+    ])
+    .then((answers) => {
+      const intern = new Intern(
+        answers.name,
+        answers.id,
+        answers.email,
+        answers.school
+      );
+      teamMembers.push(intern);
+      menu();
+    });
+};
+
+// Function to generate the HTML
+const generateHTML = () => {
+  fs.ensureDirSync(OUTPUT_DIR);
+  fs.writeFileSync(outputPath, render(teamMembers));
+};
+
+// Main menu function
+const menu = () => {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "menuOption",
+        message: "What would you like to do?",
+        choices: [
+          "Add an engineer",
+          "Add an intern",
+          "Finish building the team",
+        ],
+      },
+    ])
+    .then((userSelection) => {
+      switch (userSelection.menuOption) {
+        case "Add an engineer":
+          addEngineer();
+          break;
+        case "Add an intern":
+          addIntern();
+          break;
+        case "Finish building the team":
+          generateHTML();
+          break;
+      }
+    });
+};
+
+// Start the application
+addManager();
+
+// This code uses Inquirer to gather information about the development team members and creates objects for each team member using the correct classes as blueprints. It then calls the render function and passes in an array containing all employee objects, generates an HTML file using the HTML returned from the render function, and writes it to a file named team.html in the output folder.
--- starter/lib/Employee.js
+++ starter/lib/Employee.js
@@ -1,1 +1,17 @@
 // TODO: Write code to define and export the Employee class
+class Employee {
+    constructor(name, id, email) {
+      this.name = name;
+      this.id = id;
+      this.email = email;
+    }
+  }
+  
+  module.exports = Employee;
+
+
+  // In the above code, Employee is the parent class and Manager, Engineer, and Intern are the child classes that extend the Employee class. Each class has its own constructor that takes in the required parameters and sets the role property accordingly.
+
+// Also, you should export the class at the end of the file so that it can be imported in the main file.
+
+// You can also add the methods like getName(), getId(), getEmail(), getRole() in the Employee class and override the getRole() method in the child classes if you want.
